795772a9f97551ad86b376a6c0446a4a1f90521f
Paracells <enoalife@hotmail.com>
2020-06-04 15:48:30 +0300

    ...

---
+++ src/main/java/ru/paracells/coronavirustracker/controllers/HomeController.java
@@ -0,0 +1,28 @@
+package ru.paracells.coronavirustracker.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.paracells.coronavirustracker.models.LocationStats;
+import ru.paracells.coronavirustracker.services.CoronaVirusDataService;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(LocationStats::getLatestTotalCases).sum();
+        int totalNewCases = allStats.stream().mapToInt(LocationStats::getDiffFromPrevDay).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+        return "home";
+    }
+}
